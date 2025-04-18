0e7a76007a8a09e888c388e20fcedea54e4738ad
Chennakesava Kaja <chk017@diyarme.com>
2025-04-18 18:44:14 +0530

    mavan compiler version to 11

--- src/main/java/com/Diyar/Selenium/Framework/Lib/SelectBox.java
+++ src/main/java/com/Diyar/Selenium/Framework/Lib/SelectBox.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class SelectBox extends Select {
+   /*
    private static String locator = null;
    private static Browser browser = new Browser();
 
@@ -29,6 +30,8 @@ public class SelectBox extends Select {
    public String locator() {
       return locator;
    }
+
+   */
 }
 
 
